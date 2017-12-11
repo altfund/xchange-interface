3c3e5386fe553f09c7666ed4cb503aa9a91190d4
gpwclark <gpwclark@gmail.com>
2017-12-11 23:17:40 +0000

    ensure funding history returns currency

--- src/main/java/org/altfund/xchangeinterface/xchange/service/util/FundingRecordMixIn.java
+++ src/main/java/org/altfund/xchangeinterface/xchange/service/util/FundingRecordMixIn.java
@@ -2,18 +2,11 @@ package org.altfund.xchangeinterface.xchange.service.util;
 
 import org.knowm.xchange.currency.Currency;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public abstract class FundingRecordMixIn {
-    //MixIn(@JsonProperty("width") int w, @JsonProperty("height") int h) { }
-    //FundingRecordMixIn(@JsonManagedReference("currency") Currency currency) { }
     FundingRecordMixIn(@JsonProperty("currency") Currency currency) { }
 
-    @JsonBackReference("currency") abstract public Currency getCurrency(); // rename property
-
-    /*
-    @JsonProperty("width") abstract int getW(); // rename property
-    @JsonProperty("height") abstract int getH(); // rename property
-    @JsonIgnore int getSize(); // we don't need it!
-    */
+    @JsonManagedReference("currency") abstract public Currency getCurrency();
 }
